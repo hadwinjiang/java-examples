72e2b00bc5f37402be9aeb3bb66c8dc4ec99668d
hadwin.jiang <hadwinjiang@126.com>
2019-04-17 20:16:39 +0800

    dasboot: first controller

---
+++ das-boot/src/main/java/com/hadwin/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.hadwin.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Das Boot, reporting for duty!";
+	}
+}
