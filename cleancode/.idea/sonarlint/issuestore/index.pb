8eaff0b0f327ab4f13972f5fe2dfc815207c95e6
hadwin.jiang <hadwinjiang@126.com>
2019-04-17 19:13:39 +0800

    dasboot: initial spring template

---
+++ cleancode/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+O
+src/test/java/PerformTests.java,f\5\f5fab4a097750188b1671be39657136e36cdfbea
---
+++ cleancode/src/test/java/PerformTests.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class PerformTests {
+
+    @Test
+    public void basicMathTest(){
+        assertEquals(2, 4);
+    }
+
+}
