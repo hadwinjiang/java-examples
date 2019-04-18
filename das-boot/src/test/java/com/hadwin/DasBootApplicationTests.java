e4fcdca236e1ef1d8749ac83fe1925e8d3a3ae99
hadwin.jiang <hadwinjiang@126.com>
2019-04-18 20:47:39 +0800

    dasboot: first test with junit

--- das-boot/src/test/java/com/hadwin/DasBootApplicationTests.java
+++ das-boot/src/test/java/com/hadwin/DasBootApplicationTests.java
@@ -1,16 +1,23 @@
 package com.hadwin;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.hadwin.controllers.HomeController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DasBootApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void testHomeController() {
+		HomeController homeController = new HomeController();
+		String result = homeController.home();
+		assertEquals(result, "Das Boot, reporting for duty!");
 	}
 
 }
