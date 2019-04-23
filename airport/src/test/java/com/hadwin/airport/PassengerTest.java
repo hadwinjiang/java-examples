6e3e4977c4f7d438a5e09fa3d944488bc1653faa
hadwin.jiang <hadwinjiang@126.com>
2019-04-23 11:31:48 +0800

    airport: Test case

---
+++ airport/src/test/java/com/hadwin/airport/PassengerTest.java
@@ -0,0 +1,14 @@
+package com.hadwin.airport;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class PassengerTest {
+
+    @Test
+    public void testPassengerCreation() {
+        Passenger passenger = new Passenger("123-45-6789", "John Smith", "US");
+        assertNotNull(passenger);
+    }
+
+}
