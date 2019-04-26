c97417e8d26afd0f326d1219ca6997393c25b183
hadwinjiang@outlook.com <hadwinjiang@outlook.com>
2019-04-26 10:03:39 +0800

    eventtracker: enable both jsp and thymeleaf

---
+++ ps-event-tracker/src/main/java/com/hadwin/PsEventTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.hadwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsEventTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsEventTrackerApplication.class, args);
+	}
+
+}
