1a23a8b27f100c9b94d4c92b22f62f2e077bc0e0
hadwinjiang@outlook.com <hadwinjiang@outlook.com>
2019-04-26 11:32:30 +0800

    eventtracker: first controller with jsp view

---
+++ ps-event-tracker/src/main/java/com/hadwin/controller/EventController.java
@@ -0,0 +1,20 @@
+package com.hadwin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.hadwin.model.Event;
+
+@Controller
+public class EventController {
+
+	@RequestMapping(value = "/event", method = RequestMethod.GET)
+	public String displayEventPage(Model model) {
+		Event event = new Event();
+		event.setName("Java User Group");
+		model.addAttribute("event", event);
+		return "jsp/event";
+	}
+}
